0c7a21fc4a3824ac195a1020422171788ecd804b
acp2017 <acp3500@gmail.com>
2020-01-30 18:16:33 -0500

    Create twitterbot.py

---
+++ twitterbot.py
@@ -0,0 +1,7 @@
+import twitter
+
+def getAPI():
+	return twitter.Api(consumer_key= "pOVKVM1i"
+				 	consumer_secret= "N0uumppcsRgBo8x9rI575i2uac"
+					access_token_key= "1217212737828413448"
+					access_token_secret= "y2D5LrAgRtZjp")
